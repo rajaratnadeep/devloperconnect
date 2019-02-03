12ff71906db93ec1d88128a0d924fa68827fe24a
raja <rajaratnadeep@hotmail.com>
2019-02-03 13:31:13 +0530

    Testing registration with form axios

--- routes/api/users.js
+++ routes/api/users.js
@@ -22,7 +22,7 @@ router.get("/test", (req, res) =>
   })
 );
 
-// @route   GET api/users/register
+// @route   POST api/users/register
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
@@ -68,7 +68,7 @@ router.post("/register", (req, res) => {
   });
 });
 
-// @route   GET api/users/login
+// @route   Post api/users/login
 // @desc    Login user/ Returning JWT token
 // @access  Public
 router.post("/login", (req, res) => {
